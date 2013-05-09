1ed92d901050870dacd1fe98c0db40b8ad8aa9f4
Thiago Nunes <gorobaum@gmail.com>
2013-05-09 09:12:01 -0300

    rotacionaAsPorraToda.pi

---
+++ geo/rotacionaAsPorraToda.py
@@ -0,0 +1,26 @@
+import math
+
+class MatrizLouca:
+	def rotacionaEmX(tetaEntrada):
+		teta = tetaEntrada * 180 / math.pi
+		linha1 = [1, 0, 0, 0]
+		linha2 = [0, math.cos(self.teta), -1 * math.sin(self.teta), 0]
+		linha3 = [0, math.sin(self.teta), math.cos(self.teta), 0]
+		linha4 = [0, 0, 0, 1]
+		return m = [linha1, linha2, linha3, linha4]
+
+	def rotacionaEmY(tetaEntrada):
+		teta = tetaEntrada * 180 / math.pi
+		linha1 = [math.cos(self.teta), 0, math.sin(self.teta),0]
+		linha2 = [0, 1, 0, 0]
+		linha3 = [-1 * math.sin(self.teta), 0, math.cos(self.teta), 0]
+		linha4 = [0, 0, 0, 1]
+		return m = [linha1, linha2, linha3, linha4]
+
+	def rotacionaEmZ(tetaEntrada):
+		teta = tetaEntrada * 180 / math.pi
+		linha1 = [math.cos(self.teta), -1 * math.sin(self.teta), 0, 0]
+		linha2 = [math.sin(self.teta), math.cos(self.teta), 0, 0]
+		linha3 = [0, 0, 1, 0]
+		linha4 = [0, 0, 0, 1]
+		return m = [linha1, linha2, linha3, linha4]
