0a1507e61c4cf9b11c1e85ef3ba0579f82898a6f
Thiago Nunes <gorobaum@gmail.com>
2013-05-09 09:19:06 -0300

    translada

--- geo/rotacionaAsPorraToda.py
+++ geo/rotacionaAsPorraToda.py
@@ -24,3 +24,10 @@ class MatrizLouca:
 		linha3 = [0, 0, 1, 0]
 		linha4 = [0, 0, 0, 1]
 		return m = [linha1, linha2, linha3, linha4]
+
+	def translada(x, y, z):
+		linha1 = [1, 0, 0, x]
+		linha2 = [0, 1, 0, y]
+		linha3 = [0, 0, 1, z]
+		linha4 = [0, 0, 0, 1]
+		return m = [linha1, linha2, linha3, linha4]
