469cbd5f81082413c4eea0255c8a9f9d5b45688b
Thiago Nunes <gorobaum@gmail.com>
2013-06-05 13:12:02 -0300

    aeHO

--- geo/ponto.py
+++ geo/ponto.py
@@ -30,6 +30,7 @@ class Ponto:
         razao = (p.z - self.z ) / (p.x - self.x)
         return (x - self.x)*razao + self.z
 
-    def linearInterpolationXY(self, p, y):
+    def linearInterpolationYZ(self, p, y):
         razao = (p.z - self.z ) / (p.y - self.y)
-        return (y - self.y)*razao + self.z    
+        return (y - self.y)*razao + self.z   
+
